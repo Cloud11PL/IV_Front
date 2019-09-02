3b2dbd8d16bd29ae77a838636bbc5ab79e2c5829
Wojciech Mista <wojciech.mista@hotmail.com>
2019-09-02 21:56:03 +0200

    Significant code cleanup

--- src/scenes/components/useLoggedInStatus.js
+++ src/scenes/components/useLoggedInStatus.js
@@ -1,35 +1,7 @@
-import { useState, useEffect } from 'react';
-// import propTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-// import { createSelector } from 'reselect';
-
-// const selectAuthStatus = crea
 
 export default function useLoggedInStatus() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const { loggedIn } = useSelector((state) => state.auth.isLoggedIn);
+  const loggedIn = useSelector((state) => state.auth.isLoggedIn);
 
-  useEffect(() => {
-    if (loggedIn !== undefined && loggedIn !== null) {
-      setIsLoggedIn(loggedIn);
-    }
-    console.log('UseLoggedInStatus hir');
-    // console.log(loggedIn);
-  }, [isLoggedIn, loggedIn]);
-  return isLoggedIn;
-  // setIsLoggedIn(loggedIn);
+  return loggedIn;
 }
-
-// useLoggedInStatus.propTypes = {
-//   loggedIn: propTypes.bool,
-// };
-
-// useLoggedInStatus.defaultProps = {
-//   loggedIn: false,
-// };
-
-// const mapStateToProps = (state) => ({
-//   loggedIn: state.auth.isLoggedIn
-// });
-
-// export default connect(mapStateToProps, null)(useLoggedInStatus);
