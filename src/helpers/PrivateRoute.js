d2050a86a1fb1e5bb47fc14de82c314ea05421f1
Wojciech Mista <wojciech.mista@hotmail.com>
2019-09-02 21:53:44 +0200

    PrivateRoute improvements

--- src/helpers/PrivateRoute.js
+++ src/helpers/PrivateRoute.js
@@ -1,39 +1,28 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import propTypes from 'prop-types';
-import store from '../store';
-import useAuthDataContext from '../scenes/components/useLoggedInStatus';
+import useLoggedInStatus from '../scenes/components/useLoggedInStatus';
 import SignInPage from '../scenes/Auth/Login';
 
-// export default function PrivateRoute({ component: Component, ...rest }) {
-//   return (
-//     <Route
-//       {...rest}
-//       render={(props) => (store.getState().auth.isLoggedIn === true
-//         ? <Component {...props} />
-//         : <Redirect to={{ pathname: '/', state: { from: props.location } }} />)}
-//     />
-//   );
-// }
 let PrivateRoute;
 
 export default PrivateRoute = ({ component, ...options }) => {
-  const user = useAuthDataContext;
-  console.log('PrivateRoute mysli ze hook to', user);
-  const finalComponent = user ? component : SignInPage;
-
-  return <Route {...options} component={finalComponent} />;
+  const loggedInStatus = useLoggedInStatus();
+  if (loggedInStatus) {
+    return <Route {...options} component={component} />;
+  } 
+    return <Redirect component={SignInPage} to="/login" />;
 };
 
-// PrivateRoute.propTypes = {
-//   location: propTypes.shape({
-//     pathname: propTypes.string.isRequired,
-//   }),
-//   component: propTypes.func.isRequired
-// };
+PrivateRoute.propTypes = {
+  location: propTypes.shape({
+    pathname: propTypes.string.isRequired,
+  }),
+  component: propTypes.func.isRequired
+};
 
-// PrivateRoute.defaultProps = {
-//   location: {
-//     pathname: '/'
-//   }
-// };
+PrivateRoute.defaultProps = {
+  location: {
+    pathname: '/'
+  }
+};
