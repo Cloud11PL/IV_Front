f16386f5afe0ae5171b3874663f8f1a85838bf03
Wojciech Mista <wojciech.mista@hotmail.com>
2019-09-02 21:52:45 +0200

    Added LogOut route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import store from './store/index';
 import { authUser } from './actions/authActions';
@@ -12,19 +12,23 @@ import Home from './scenes/Home/Home';
 import Login from './scenes/Auth/Login';
 import Dashboard from './scenes/Dashboard';
 
+import LogOut from './scenes/Auth/Login/components/LogOut';
+
 const user = JSON.parse(localStorage.getItem('user'));
 
 if (user && user.token) {
   store.dispatch(authUser(user.token));
-  console.log(user);
 }
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <PrivateRoute path="/dashboard" component={Dashboard} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/logout" component={LogOut} />
+      </Switch>
     </Router>
   </Provider>,
   document.getElementById('root')
