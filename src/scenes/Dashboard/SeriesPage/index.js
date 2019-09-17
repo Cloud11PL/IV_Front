398e22ec380ac28bd35c3dfe8cf5f7803ed4b2ab
Wojciech Mista <wojciech.mista@hotmail.com>
2019-09-17 15:57:50 +0200

    Basic series page

---
+++ src/scenes/Dashboard/SeriesPage/index.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+import Chart from 'react-apexcharts';
+import propTypes from 'prop-types';
+import API from '../../../_config/axios';
+import chartOptions from './chartOptions';
+
+import NavBar from '../../components/NavBar';
+import Modal from '../../components/Modal';
+
+function SeriesPage({ match, location }) {
+  const [seriesForChart, setSeriesForChart] = useState([]);
+  const [seriesDetails, setSeriesDetails] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
+  function handleChange(e) {
+    e.persist();
+    setSeriesDetails((data) => ({ ...data, [e.target.id]: e.target.value }));
+  }
+
+  function handleClick() {
+    setShowModal(!showModal);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleClick();
+    console.log(seriesDetails);
+  }
+
+  const seriesId = match.params.id;
+  const mqttName = match.params.device;
+
+  useEffect(() => {
+    setSeriesDetails(location.state);
+  }, [location.state]);
+
+  function convertAPIData(series) {
+    const tempSeries = [];
+    series.map((singleInput) => 
+      tempSeries.push({
+        x: new Date(singleInput.Time).getTime(),
+        y: singleInput.payload,
+      }));
+    setSeriesForChart([{ data: tempSeries }]);
+  }
+
+  useEffect(() => {
+    async function fetchSeriesData() {
+      await API.get('http://localhost:1200/series', {
+        params: {
+          mqttName,
+          seriesId,
+        }
+      }).then((nodeData) => {
+        convertAPIData(nodeData.data);
+      });
+    }
+    fetchSeriesData();
+  }, [mqttName, seriesId]);
+
+
+  return (
+    <div>
+      <NavBar />
+      We are now looking at a nice page for a device:
+      <br />
+      <h1>
+        {`Device name: ${match.params.device}`}
+      </h1>
+      <h1> 
+        {`Series ID: ${match.params.id}`}
+      </h1>
+      {seriesDetails 
+        && (
+        <h1>
+          {`Bag type: ${seriesDetails.BagType}`}
+          <button type="button" onClick={handleClick}>Edit</button>
+          <Modal open={showModal} onClose={handleClick} className="modal">
+            <label>Bag type:</label>
+            <input 
+              placeholder={seriesDetails.BagType}
+              id="BagType"
+              onChange={handleChange}
+            />
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+          </Modal>
+          <br />
+          {`Status: ${seriesDetails.isDone ? 'Finished' : 'Not finished'}`}
+        </h1>
+      )}
+      {seriesForChart.length > 0 && <Chart options={chartOptions} series={seriesForChart} type="line" height="350" />}
+    </div>
+  );
+}
+
+SeriesPage.propTypes = {
+  match: propTypes.shape({
+    params: propTypes.shape({
+      id: propTypes.node,
+      device: propTypes.node,
+    }).isRequired,
+  }).isRequired,
+  location: propTypes.shape({
+    state: propTypes.shape({
+      BagType: propTypes.string.isRequired,
+      Device_Id: propTypes.string.isRequired,
+      _id: propTypes.string.isRequired,
+      time: propTypes.string.isRequired,
+      isDone: propTypes.bool.isRequired,
+      SeriesId: propTypes.number.isRequired,
+    }).isRequired
+  }).isRequired
+};
+
+
+export default withRouter(SeriesPage);
