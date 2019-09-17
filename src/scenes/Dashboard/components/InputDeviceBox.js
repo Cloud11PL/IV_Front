f06ece62393f800d60dd0d7bc63a7b86524fa9e9
Wojciech Mista <wojciech.mista@hotmail.com>
2019-09-17 15:59:26 +0200

    Component fixes

--- src/scenes/Dashboard/components/InputDeviceBox.js
+++ src/scenes/Dashboard/components/InputDeviceBox.js
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import DeviceWebsocket from './DeviceWebsocketConnection';
+import GetSeries from './GetSeries';
+
 
 export default function InputDeviceBox(props) {
   const [deviceData, setDeviceData] = useState({});
+  const [isClicked, setClicked] = useState(false);
+  const clicked = false;
   
   useEffect(() => {
     const { device } = props;
     setDeviceData(device);
   }, [props]);
 
+  function handleClick() {
+    console.log('Device ID =>', deviceData._id);
+    console.log('Click status', clicked);
+    // clicked = !clicked;
+    setClicked(!isClicked);
+  }
+
   return (
     <div className="device__live-input-box">
-      <p className="device__live-input-box__text">
+      <div className="device__live-input-box__text">
         {deviceData.mqttName}
         <DeviceWebsocket mqttName={deviceData.mqttName} />
-      </p>
+        <button onClick={handleClick} type="button">Get series</button>
+        
+        {isClicked && <GetSeries id={deviceData._id} mqttName={deviceData.mqttName} />}
+      </div>
     </div>
   );
 }
