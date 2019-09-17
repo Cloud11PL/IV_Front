c4d8396fa243317e7558e43d25e8487cbef12e2e
Wojciech Mista <wojciech.mista@hotmail.com>
2019-09-17 15:58:06 +0200

    Component fixes

--- src/scenes/Dashboard/components/DeviceWebsocketConnection.js
+++ src/scenes/Dashboard/components/DeviceWebsocketConnection.js
@@ -1,19 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:1200');
 
 export default function DeviceWebsocketConnection(props) {
   const [currentInput, setCurrentInput] = useState(0);
+  const [connectionName, setConnectionName] = useState(null);
+  const [isConnected, setConnected] = useState(false);
 
-  const { mqttName } = props;
+  useEffect(() => {
+    const { mqttName } = props;
+    if (mqttName !== undefined) {
+      setConnectionName(mqttName);
+      setConnected(true);
+    }
+  }, [isConnected, props]);
 
-  if (mqttName !== undefined) {
-    const socket = io.connect('http://localhost:1200');
-    socket.emit('join', `${mqttName}`);
-    socket.on(`${mqttName}`, (data) => {
-      console.log(data);
-      setCurrentInput(data);
+
+  // Join the room
+  useEffect(() => {
+    if (connectionName !== undefined && isConnected) {
+      socket.emit('join', connectionName);
+      return () => {
+        if (isConnected) {
+          console.log('Websocket Closed for some reason');
+        }
+      };
+    }
+  }, [connectionName, isConnected]);
+
+  // Recieve message
+  useEffect(() => {
+    socket.on(connectionName, (data) => {
+      if (data !== currentInput) {
+        setCurrentInput(data);
+      }
     });
-  }
+  }, [connectionName, currentInput]);
 
   const inputToDisplay = () => {
     if (currentInput === 0) {
