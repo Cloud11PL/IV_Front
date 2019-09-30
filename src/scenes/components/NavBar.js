8b7a5ea2aaa8690229867a105eb2d50f05cc62c5
Wojciech Mista <wojciech.mista@hotmail.com>
2019-09-30 10:21:47 +0200

    Added icons for navbar buttons

--- src/scenes/components/NavBar.js
+++ src/scenes/components/NavBar.js
@@ -1,22 +1,44 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import homeicon from './assets/home-icon.png';
+import loginicon from './assets/logout-icon.png';
+import logouticon from './assets/logout-icon.png';
+import dashboardicon from './assets/dashboard-icon.png';
+
 import useLoggedInStatus from './useLoggedInStatus';
+
 
 export default function NavBar() {
   useEffect(() => {
   });
   const isLoggedIn = useLoggedInStatus();
   return (
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        {!isLoggedIn 
-          && <li><Link to="/login">Login</Link></li>}
-        {isLoggedIn 
-          && <li><Link to="/dashboard">Dashboard</Link></li>}
-        {isLoggedIn 
-          && <li><Link to="/logout">Log Out</Link></li>}
-      </ul>
-    </div>
+    <aside className="navbar">
+      <NavLink to="/" className="navbar__item" exact activeClassName="navbar__item--active">
+        <img src={homeicon} alt="home-icon" className="navbar__icon" />
+        <p className="navbar__text">Home</p>
+      </NavLink>
+      {!isLoggedIn 
+          && (
+            <NavLink to="/login" className="navbar__item" activeClassName="navbar__item--active">
+              <img src={loginicon} alt="login-icon" className="navbar__icon" />
+              <p className="navbar__text">Login</p>
+            </NavLink>
+          )}
+      {isLoggedIn 
+          && (
+            <NavLink to="/dashboard" className="navbar__item" activeClassName="navbar__item--active">
+              <img src={dashboardicon} alt="dashboard-icon" className="navbar__icon" />
+              <p className="navbar__text">Dashboard</p>
+            </NavLink>
+          )}
+      {isLoggedIn 
+          && (
+            <NavLink to="/logout" className="navbar__item" activeClassName="navbar__item--active">
+              <img src={logouticon} alt="dashboard-icon" className="navbar__icon" />
+              <p className="navbar__text">Log out</p>
+            </NavLink>
+          )}
+    </aside>
   );
 }
